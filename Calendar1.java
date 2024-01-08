631dd6c63fe5fd4ad5cf383aa8371b83f2b82ed3
DanaLevi005 <danapearl.levi@post.runi.ac.il>
2024-01-08 21:59:23 +0200

    hw

--- Calendar1.java
+++ Calendar1.java
@@ -34,9 +34,7 @@ public class Calendar1 {
 	 		debugDaysCounter++;
 	 		//// If you want to stop the loop after n days, replace the condition of the
 	 		//// if statement with the condition (debugDaysCounter == n)
-	 		if (debugDaysCounter == 500) { 
-	 			break;
-	 		}
+	 		
         }
 	 	System.out.println("During the 20th century, " + sundayCounter + " Sundays fell on the first day of the month");
 	 }
